e6fbd26790c2da58508246999304af498cf1b4dd
Shree Shashank.S <darkeagle9247@gmail.com>
2025-07-28 15:38:40 +0000

    its showing theres no nextjs file to run in localhost

--- next.config.ts
+++ next.config.ts
@@ -24,9 +24,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA({
+const pwaConfig = {
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-})(nextConfig);
+};
+
+export default withPWA(pwaConfig)(nextConfig);
